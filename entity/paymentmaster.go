f851ccd84909fc4e384ef29a377f68bff40d03c5
Niran Wangyad <niran.w@cicnetgrp.net>
2026-03-29 15:09:10 +0700

    Add missing fields to entity structs to fix build errors

--- entity/paymentmaster.go
+++ entity/paymentmaster.go
@@ -12,4 +12,9 @@ type PaymentMaster struct {
 	BranchId   uint64    `json:"branch_id"`
 	CratedBy   uint64    `json:"crated_by"`
 	CreatedAt  uint64    `json:"created_at"`
+	SlipDoc    string    `json:"slip_doc"`
+}
+
+type SlipDoc struct {
+	Image []string `json:"image"`
 }
